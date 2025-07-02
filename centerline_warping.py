9f7269decfe2f37e278177c111611e3bf28691f4
Brandon Hardy <59355744+b-k-hardy@users.noreply.github.com>
2025-07-02 18:38:33 -0400

    removing interactive notebook comments, adding features to assess (s, theta) coordinate system, and other things that i need to remove but I want them in the commit history

--- centerline_warping.py
+++ centerline_warping.py
@@ -1,9 +1,9 @@
-# %%
 import matplotlib.pyplot as plt
 import numpy as np
 import pyvista as pv
 import scienceplots
 from scipy.interpolate import make_splprep
+from scipy.spatial.transform import Rotation as R
 
 
 def surface_ray_tracing_displacement(mesh_d: pv.PolyData, mesh_s: pv.PolyData) -> pv.PolyData:
@@ -46,7 +46,7 @@ def pyvista_surface_nearest_neighbor_mapping(mesh_d: pv.PolyData, mesh_s: pv.Pol
     displacements = closest_points - mesh_d.points
     mesh_d.point_data.set_vectors(
         displacements,
-        "Distance to Nearest Implicit Surface Point PYVISTA",
+        "Nearest Surface Point Displacement",
     )
     mesh_d = mesh_d.warp_by_vector("Centerline Displacement", factor=-1.0)  # Reverse the warping direction
 
@@ -68,6 +68,7 @@ def centerline_nearest_neighbor_mapping(
     # loop through the mesh points and find the nearest centerline point. Take values at that point and assign to the mesh point.
     mesh_circularity = np.zeros(mesh.n_points)
     mesh_arc_length = np.zeros(mesh.n_points)
+    mesh_circ_coordinate = np.zeros(mesh.n_points)
     centerline_displacement = np.zeros((mesh.n_points, 3))
     additional_displacement = np.zeros((mesh.n_points, 3))
     mesh_circularity.fill(np.nan)  # fill with NaN to make sure points that aren't assigned stand out when debugging
@@ -83,14 +84,74 @@ def centerline_nearest_neighbor_mapping(
         # assign the circularity and arc length values from the centerline to the mesh point
         mesh_circularity[i] = centerline_spline["Circularity"][centerline_idx]
         mesh_arc_length[i] = centerline_spline["arc_length"][centerline_idx]
+        circ_coordinate = mesh_point - centerline_spline.points[centerline_idx]
+        # project circ_coordinate onto the centerline derivative vector to get the circumferential coordinate
+        circ_derivative = centerline_spline["Derivative"][centerline_idx]
+        circ_derivative /= np.linalg.norm(circ_derivative)  # normalize the derivative vector
+        circ_coordinate = (
+            circ_coordinate
+            - np.dot(circ_coordinate, circ_derivative) / np.linalg.norm(circ_derivative) ** 2 * circ_derivative
+        )
+        circ_coordinate = np.arctan2(
+            np.dot(circ_coordinate, np.cross(circ_derivative, centerline_spline["zero_vector"][centerline_idx])),
+            np.dot(circ_coordinate, centerline_spline["zero_vector"][centerline_idx]),
+        )
+        mesh_circ_coordinate[i] = circ_coordinate
         centerline_displacement[i] = centerline_spline["Displacement"][centerline_idx]
         additional_displacement[i] = mesh["Displacement"][i] - centerline_spline["Displacement"][centerline_idx]
 
     # assign the circularity and arc length values to the mesh point data
+    mesh.point_data.set_array(mesh_circ_coordinate, "Circumferential Coordinate")
     mesh.point_data.set_array(mesh_circularity, "Circularity")
     mesh.point_data.set_array(mesh_arc_length, "arc_length")
     mesh.point_data.set_array(centerline_displacement, "Centerline Displacement")
     mesh.point_data.set_array(additional_displacement, "Additional Displacement")
+
+
+def centerline_nearest_neighbor_mapping_systole(
+    centerline_spline: pv.PolyData,
+    mesh: pv.PolyData,
+) -> None:
+    """Run this function AFTER making centerline spline and finding circularity and arc length.
+
+    Args:
+        centerline_spline (pv.PolyData): _description_
+        mesh (pv.PolyData): _description_
+
+    """
+    # loop through the mesh points and find the nearest centerline point. Take values at that point and assign to the mesh point.
+    mesh_circularity = np.zeros(mesh.n_points)
+    mesh_arc_length = np.zeros(mesh.n_points)
+    mesh_circ_coordinate = np.zeros(mesh.n_points)
+    mesh_circularity.fill(np.nan)  # fill with NaN to make sure points that aren't assigned stand out when debugging
+    mesh_arc_length.fill(np.nan)
+
+    for i, mesh_point in enumerate(mesh.points):
+        # find the nearest centerline point
+        centerline_points = centerline_spline.points
+        # calculate the distance from the node to each centerline point and find the index of the closest centerline point
+        centerline_idx = np.argmin(np.linalg.norm(centerline_points - mesh_point, axis=1))
+        # assign the circularity and arc length values from the centerline to the mesh point
+        mesh_circularity[i] = centerline_spline["Circularity"][centerline_idx]
+        mesh_arc_length[i] = centerline_spline["arc_length"][centerline_idx]
+        circ_coordinate = mesh_point - centerline_spline.points[centerline_idx]
+        # project circ_coordinate onto the centerline derivative vector to get the circumferential coordinate
+        circ_derivative = centerline_spline["Derivative"][centerline_idx]
+        circ_derivative /= np.linalg.norm(circ_derivative)  # normalize the derivative vector
+        circ_coordinate = (
+            circ_coordinate
+            - np.dot(circ_coordinate, circ_derivative) / np.linalg.norm(circ_derivative) ** 2 * circ_derivative
+        )
+        circ_coordinate = np.arctan2(
+            np.dot(circ_coordinate, np.cross(circ_derivative, centerline_spline["zero_vector"][centerline_idx])),
+            np.dot(circ_coordinate, centerline_spline["zero_vector"][centerline_idx]),
+        )
+        mesh_circ_coordinate[i] = circ_coordinate
+
+    # assign the circularity and arc length values to the mesh point data
+    mesh.point_data.set_array(mesh_circ_coordinate, "Circumferential Coordinate")
+    mesh.point_data.set_array(mesh_circularity, "Circularity")
+    mesh.point_data.set_array(mesh_arc_length, "arc_length")
 
 
 def calc_circularity(area: float, perimeter: float) -> float:
@@ -141,9 +202,12 @@ def circularity_contours(
     centerline_sp: pv.PolyData,
     clip_radius: float = 35.0,
 ) -> pv.PolyData:
+    # holy fuck these hints are wrong
+    # let's get a little silly and add a rotating mohawk
     extracted_cuts = []
     extracted_surfaces = []
     circularity_array = []
+    circumferential_zero_vectors = []
 
     for i in range(centerline_sp.n_points):
         roi = pv.Sphere(center=centerline_sp.points[i], radius=clip_radius)
@@ -165,24 +229,55 @@ def circularity_contours(
         extracted_cuts.append(extracted)
         extracted_surfaces.append(extracted_surf)
 
+        circ0 = np.cross(centerline_sp["Derivative"][i], np.array([-1, 0, 0]))
+        circumferential_zero_vectors.append(circ0 / np.linalg.norm(circ0))
+
     contours = pv.merge(extracted_cuts)
     surfaces = pv.merge(extracted_surfaces)
 
     # NOTE: potentially return u from make_splprep to use for plotting... need to figure out what it means EXACTLY
-    return contours, surfaces, np.array(circularity_array)
+    return contours, surfaces, np.array(circularity_array), np.array(circumferential_zero_vectors)
 
 
-# %%
+def circumferential_contours(centerline_sp: pv.PolyData) -> pv.PolyData:
+    circumferential_contours = []
+    circ_fans = []
+    for i in range(centerline_sp.n_points):
+        # create a list of vectors that are perpendicular to the centerline and rotate around the centerline point starting from the zero vector
+        circ0 = centerline_sp["zero_vector"][i]
+        circ0 /= np.linalg.norm(circ0)  # should already be normalized, but good to be sure
+        rot_vector = centerline_sp["Derivative"][i] / np.linalg.norm(
+            centerline_sp["Derivative"][i],
+        )  # normalize the derivative vector
+        all_points = (
+            R.from_rotvec(
+                np.linspace(0, 2 * np.pi, 100)[:, None] * rot_vector,  # rotate around the centerline tangent vector,
+            ).apply(circ0)
+            * 10
+            + centerline_sp.points[i]
+        )
+        vectors = all_points - centerline_sp.points[i]
+        vectors /= np.linalg.norm(vectors, axis=1)[:, None]  # normalize the vectors
+        circumferential_contours.append(
+            pv.PolyData(all_points, lines=[all_points.shape[0]] + np.arange(all_points.shape[0]).tolist()),
+        )
+        circ_fan = pv.PolyData(np.repeat(centerline_sp.points[i][None, :], all_points.shape[0], axis=0))
+        # circ_fan["circumferential_vectors"] = vectors
+        circ_fan.point_data.set_vectors(vectors, "Circumferential Vectors")
+        circ_fans.append(circ_fan)
+    return pv.merge(circumferential_contours), pv.merge(circ_fans, merge_points=False)
+
+
 def main():
-    # %%
     # load the data
-    centerline_diastole = pv.read("David/Centerline_model_David.vtp")
-    centerline_systole = pv.read("David/Centerline warped.vtp")
+    data_directory = "David"
+
+    centerline_diastole = pv.read(f"{data_directory}/Centerline_model_David.vtp")
+    centerline_systole = pv.read(f"{data_directory}/Centerline warped.vtp")
     # tried to reverse the systole centerline points but for whatever reason it's actually fucking impossible
-    full_surface_diastole = pv.read("David/david_vdm.vtp")
-    full_surface_systole = pv.read(
-        "David/david_vdm_warped.vtp",
-    )  # could also just warp by vector on diastole but oh well
+    full_surface_diastole = pv.read(f"{data_directory}/david_vdm.vtp")
+    full_surface_systole = pv.read(f"{data_directory}/david_vdm_warped.vtp")
+    # could also just warp by vector on diastole but oh well
 
     # Fit splines to the centerline points
     centerline_diastole_spline = fit_spline(centerline_diastole)
@@ -194,66 +289,144 @@ def main():
     centerline_displacement = centerline_systole_spline.points - centerline_diastole_spline.points
     centerline_diastole_spline.point_data.set_vectors(centerline_displacement, "Displacement")
 
-    all_extracted_diastole, all_surfaces_diastole, circularity_diastole = circularity_contours(
-        full_surface_diastole,
-        centerline_diastole_spline,
-        clip_radius=35.0,
+    all_extracted_diastole, all_surfaces_diastole, circularity_diastole, circumferential_zero_vectors_diastole = (
+        circularity_contours(
+            full_surface_diastole,
+            centerline_diastole_spline,
+            clip_radius=50.0,
+        )
     )
-    all_extracted_systole, all_surfaces_systole, circularity_systole = circularity_contours(
-        full_surface_systole,
-        centerline_systole_spline,
-        clip_radius=38.0,
+    all_extracted_systole, all_surfaces_systole, circularity_systole, circumferential_zero_vectors_systole = (
+        circularity_contours(
+            full_surface_systole,
+            centerline_systole_spline,
+            clip_radius=50.0,
+        )
     )
 
-    centerline_diastole_spline.point_data.set_scalars(circularity_diastole, "Circularity")
-    centerline_systole_spline.point_data.set_scalars(circularity_systole, "Circularity")
+    centerline_diastole_spline["Circularity"] = circularity_diastole
+    centerline_systole_spline["Circularity"] = circularity_systole
+    centerline_diastole_spline["zero_vector"] = circumferential_zero_vectors_diastole
+    centerline_systole_spline["zero_vector"] = circumferential_zero_vectors_systole
+
+    diastole_circumferential_contours, diastole_fan = circumferential_contours(centerline_diastole_spline)
+    systole_circumferential_contours, systole_fan = circumferential_contours(centerline_systole_spline)
 
     centerline_nearest_neighbor_mapping(centerline_diastole_spline, full_surface_diastole)
+    centerline_nearest_neighbor_mapping_systole(
+        centerline_systole_spline,
+        full_surface_systole,
+    )  # NOTE: a ton of this is not necessary. Need to split this function for systole and diastole...
     full_surface_diastole = pyvista_surface_nearest_neighbor_mapping(full_surface_diastole, full_surface_systole)
-    full_surface_diastole = surface_ray_tracing_displacement(full_surface_diastole, full_surface_systole)
+    # full_surface_diastole = surface_ray_tracing_displacement(full_surface_diastole, full_surface_systole)
     # centerline_nearest_neighbor_mapping(centerline_systole_spline, full_surface_systole)
 
-    centerline_diastole_spline.save("David/David_spline_centerline_diastole.vtp")
-    centerline_systole_spline.save("David/David_spline_centerline_systole.vtp")
-
-    all_extracted_diastole.save("David/david_diastole_contours.vtp")
-    all_surfaces_diastole.save("David/david_diastole_cross_sections.vtp")
-
-    all_extracted_systole.save("David/david_systole_contours.vtp")
-    all_surfaces_systole.save("David/david_systole_cross_sections.vtp")
-
-    full_surface_diastole.save(
-        "David/david_diastole_surface_ray_tracing.vtp",
+    s = full_surface_diastole["arc_length"] / centerline_diastole_spline["arc_length"][-1]
+    theta = (full_surface_diastole["Circumferential Coordinate"] + np.pi) / (2 * np.pi)
+    full_surface_diastole.point_data.set_array(
+        np.stack((s, theta), axis=1),
+        name="surface_coordinates",
     )
-    # NOTE: CHANGE NAME OF OLD FILE IF YOU WANT TO KEEP IT
-    # full_surface_systole.save("David/david_systole_surface.vtp")
+    full_surface_diastole = full_surface_diastole.compute_derivative(scalars="surface_coordinates")
+    s_grad = full_surface_diastole["gradient"][:, :3]
+    theta_grad = full_surface_diastole["gradient"][:, 3:]
+    full_surface_diastole.point_data.set_vectors(s_grad, name="s_gradient")
+    full_surface_diastole.point_data.set_vectors(theta_grad, name="theta_gradient")
+
+    centerline_diastole_spline.save(f"{data_directory}/David_spline_centerline_diastole.vtp")
+    centerline_systole_spline.save(f"{data_directory}/David_spline_centerline_systole.vtp")
+
+    all_extracted_diastole.save(f"{data_directory}/david_diastole_contours.vtp")
+    diastole_circumferential_contours.save(f"{data_directory}/david_diastole_circumferential_contours.vtp")
+    diastole_fan.save(f"{data_directory}/david_diastole_circumferential_fans.vtp")
+    all_surfaces_diastole.save(f"{data_directory}/david_diastole_cross_sections.vtp")
+
+    all_extracted_systole.save(f"{data_directory}/david_systole_contours.vtp")
+    systole_circumferential_contours.save(f"{data_directory}/david_systole_circumferential_contours.vtp")
+    systole_fan.save(f"{data_directory}/david_systole_circumferential_fans.vtp")
+    all_surfaces_systole.save(f"{data_directory}/david_systole_cross_sections.vtp")
+
+    full_surface_diastole.save(f"{data_directory}/david_diastole_surface.vtp")
+    full_surface_systole.save("David/david_systole_surface.vtp")
+
+    full_surface_diastole_warped = full_surface_diastole.warp_by_vector("Centerline Displacement")
+    full_surface_diastole_warped = full_surface_diastole_warped.warp_by_vector("Nearest Surface Point Displacement")
+
+    # all_extracted_dwarp, all_surfaces_dwarp, circularity_dwarp, circumferential_zero_vectors_dwarp = (
+    #    circularity_contours(
+    #        full_surface_diastole_warped,
+    #        centerline_systole_spline,
+    #        clip_radius=50.0,
+    #    )
+    # )
+
+    # centerline_systole_spline["zero_vector"] = circumferential_zero_vectors_dwarp
+    # centerline_systole_spline["Circularity"] = circularity_dwarp
+
+    # centerline_nearest_neighbor_mapping_systole(centerline_systole_spline, full_surface_diastole_warped)
+
+    # s = full_surface_diastole_warped["arc_length"] / centerline_diastole_spline["arc_length"][-1]
+    # theta = (full_surface_diastole_warped["Circumferential Coordinate"] + np.pi) / (2 * np.pi)
+    # full_surface_diastole_warped.point_data.set_array(
+    #    np.stack((s, theta), axis=1),
+    #    name="surface_coordinates",
+    # )
+    full_surface_diastole_warped = full_surface_diastole_warped.compute_derivative(scalars="surface_coordinates")
+    s_grad = full_surface_diastole_warped["gradient"][:, :3]
+    theta_grad = full_surface_diastole_warped["gradient"][:, 3:]
+    full_surface_diastole_warped.point_data.set_vectors(s_grad, name="s_gradient")
+    full_surface_diastole_warped.point_data.set_vectors(theta_grad, name="theta_gradient")
+
+    full_surface_diastole_warped.save(f"{data_directory}/david_diastole_surface_warped.vtp")
 
     # NEXT: need a way to associate the surface nodes with the centerline nodes... nearest neighbor for now? Then check in paraview
     # NOTE: I kind of do this already in a different script... just find that and copy?
 
-    # %%
+    if PLOT:
+        with plt.style.context(["science", "notebook"]):
+            fig, ax = plt.subplots(figsize=(8, 4), layout="constrained")
+            ax.set_title("Circularity of Aortic Cross-Sections")
 
-    with plt.style.context(["science", "notebook"]):
-        fig, ax = plt.subplots(figsize=(8, 4), layout="constrained")
-        ax.set_title("Circularity of Aortic Cross-Sections")
+            ax.plot(
+                centerline_diastole_spline["arc_length"] / centerline_diastole_spline["arc_length"][-1],
+                circularity_diastole,
+                label=f"Diastole (length = {centerline_diastole_spline['arc_length'][-1]:.2f} mm)",
+            )
+            ax.plot(
+                centerline_systole_spline["arc_length"] / centerline_systole_spline["arc_length"][-1],
+                circularity_systole,
+                label=f"Systole (length = {centerline_systole_spline['arc_length'][-1]:.2f} mm)",
+            )
+            ax.set_xlabel("Normalized Arc Length")
+            ax.set_ylabel("Circularity")
 
-        ax.plot(
-            centerline_diastole_spline["arc_length"] / centerline_diastole_spline["arc_length"][-1],
-            circularity_diastole,
-            label=f"Diastole (length = {centerline_diastole_spline['arc_length'][-1]:.2f} mm)",
-        )
-        ax.plot(
-            centerline_systole_spline["arc_length"] / centerline_systole_spline["arc_length"][-1],
-            circularity_systole,
-            label=f"Systole (length = {centerline_systole_spline['arc_length'][-1]:.2f} mm)",
-        )
-        ax.set_xlabel("Normalized Arc Length")
-        ax.set_ylabel("Circularity")
+            ax.legend()
+            # plt.show()
 
-        ax.legend()
+        with plt.style.context(["science", "notebook"]):
+            fig, ax = plt.subplots(figsize=(8, 4), layout="constrained")
+            ax.set_title("Surface Unwrapped")
+
+            ax.scatter(
+                full_surface_diastole["Circumferential Coordinate"],
+                full_surface_diastole["arc_length"],
+                # label="Diastole",
+                # color="blue",
+            )
+            # ax.scatter(
+            #    centerline_systole_spline["arc_length"] / centerline_systole_spline["arc_length"][-1],
+            #    circularity_systole,
+            #    label="Systole",
+            #    color="red",
+            # )
+            ax.set_xlabel("Circumferential Coordinate (radians)")
+            ax.set_ylabel("Arc Length (mm)")
+
+            # ax.legend()
+
         plt.show()
 
 
-# %%
 if __name__ == "__main__":
+    PLOT = False  # Set to True if you want to plot the results
     main()
