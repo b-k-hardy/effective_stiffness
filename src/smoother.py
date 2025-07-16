ba905ba348989ddd9b230a5aac3c0b30b7bfce78
Brandon Hardy <59355744+b-k-hardy@users.noreply.github.com>
2025-07-16 13:37:21 -0400

    smoother update

--- src/smoother.py
+++ src/smoother.py
@@ -1,4 +1,5 @@
-# Smoothing functions for effective stiffness analysis
+"""Iterative Laplacian Smoothing for Meshes."""
+
 import itertools
 from typing import Literal
 
@@ -13,62 +14,56 @@ class DisplacementSmoother:
 
     def __init__(
         self,
-        mesh: pv.PolyData,
+        diastolic_mesh: pv.PolyData,
+        systolic_mesh: pv.PolyData,
         n_neighbors: int = 1,
         weighting_method: Literal["uniform", "squared"] = "uniform",
     ) -> None:
         """Initialize the displacement smoother.
 
-        This method initializes the smoother with a mesh and builds the combinatorial Laplacian matrix for the first pass.
-        # NOTE: consider removing matrix builder and turn into a more dynamic approach
+        This method initializes the smoother with the combinatorial Laplacian matrix for the first pass.
 
         Args:
-            mesh (pv.PolyData): The input mesh.
+            diastolic_mesh (pv.PolyData): The diastolic mesh to smooth.
+            systolic_mesh (pv.PolyData): The systolic mesh to align with.
             n_neighbors (int, optional): The number of neighbors to consider for smoothing. Defaults to 1.
             weighting_method (Literal["uniform", "squared"], optional): The weighting method to use. Defaults to "uniform".
 
         """
-        self.orig_mesh = mesh  # Store the original mesh for reference
-        self.mesh = mesh.copy()  # make a copy of the mesh to avoid modifying the original
+        self.mesh = diastolic_mesh.copy()  # make a copy of the mesh to avoid modifying the original
+        self.systolic_mesh = systolic_mesh
         self.residuals = []
+        self.weighting_method = weighting_method
+        self.n_neighbors = n_neighbors
 
         # Initialize the laplacian matrix. Guess what: (FUCK) this thing needs to be updating
         # After each major iteration... (not the iterative method's subiterations with relax factor)
-        self.laplacian_matrix = DisplacementSmoother._build_combinatorial_laplacian(
-            mesh,
-            n_neighbors,
-            weighting_method,
-        )
+        self.laplacian_matrix = self._build_combinatorial_laplacian()
 
-    @staticmethod
-    def _build_combinatorial_laplacian(
-        mesh: pv.PolyData,
-        n_neighbors: int,
-        weighting_method: Literal["uniform", "squared"] = "uniform",
-    ) -> sp.csr_matrix:
-        n = len(mesh.points)
+    def _build_combinatorial_laplacian(self) -> sp.csr_matrix:
+        n = len(self.mesh.points)
         rows, cols, diag, data = [], [], [], []
 
         # Loop over all edges
-        for i in range(len(mesh.points)):
+        for i in range(len(self.mesh.points)):
             inner_data = []
-            neighbor_points_idx = mesh.point_neighbors_levels(i, n_neighbors)
+            neighbor_points_idx = self.mesh.point_neighbors_levels(i, self.n_neighbors)
             neighbor_points_idx = list(itertools.chain.from_iterable(neighbor_points_idx))
             for j in neighbor_points_idx:
                 rows.append(i)
                 cols.append(j)
 
-                if weighting_method == "squared":
-                    inner_data.append(-1 / (np.linalg.norm(mesh.points[i] - mesh.points[j] + 0.05) ** 2))
+                if self.weighting_method == "squared":
+                    inner_data.append(-1 / (np.linalg.norm(self.mesh.points[i] - self.mesh.points[j] + 0.05) ** 2))
 
-            if weighting_method == "uniform":
+            if self.weighting_method == "uniform":
                 diag.append(len(neighbor_points_idx))
-            elif weighting_method == "squared":
+            elif self.weighting_method == "squared":
                 data.extend(inner_data)
                 diag.append(-np.sum(inner_data))
 
         # Off-diagonal entries: -1 for each neighbor
-        if weighting_method == "uniform":
+        if self.weighting_method == "uniform":
             data = -np.ones(len(rows))
         L = sp.coo_matrix((data, (rows, cols)), shape=(n, n))
 
@@ -83,22 +78,32 @@ class DisplacementSmoother:
         max_iter: int = 100,
         weight: float = 1000.0,
         atol: float = 1e-6,
-    ) -> None:
+    ) -> pv.PolyData:
         """Loop over the smoothing process."""
         # This method can be used to iterate over the smoothing process
         # and apply the smoothing to the mesh points.
         self.residuals.clear()  # clear residuals if you'd like to re-run with different weighting
+        self.residuals.append(np.inf)  # initialize with a large value
 
-        while len(self.residuals) < max_iter and self.residuals[-1] > atol:
-            smoothed_mesh = self.inner_laplacian_smoothing(
-                self.mesh,
-                weight=self.laplacian,
-                n_neighbors=1,
-                weighting_method="uniform",
+        while len(self.residuals) - 1 < max_iter + 1 and self.residuals[-1] > atol:
+            old_points = self.mesh.points.copy()
+            intermediate_mesh = self.inner_laplacian_smoothing(weight=weight)
+            _, closest_points = self.systolic_mesh.find_closest_cell(
+                intermediate_mesh.points,
+                return_closest_point=True,
             )
+            self.mesh.points = closest_points
+            # Calculate the residuals
+            distance = np.linalg.norm(old_points - self.mesh.points, axis=1)
+            residual = np.linalg.norm(distance)
+            print(f"Iteration {len(self.residuals) - 1}: ||x^(n-1) - x^n|| = {residual:.6f}")
+            self.residuals.append(residual)
+            # has to be rebuilt after each iteration if we do anything other than uniform weighting
+            if self.weighting_method != "uniform":
+                self.laplacian_matrix = self._build_combinatorial_laplacian()
 
-    def smoother_analysis(self):
-        """Perform analysis on the smoother."""
+        self.residuals = np.array(self.residuals[1:])  # remove the initial inf value
+        return self.mesh
 
     def inner_laplacian_smoothing(self, weight: float = 1000.0) -> pv.PolyData:
         """Perform Laplacian smoothing on a mesh using a matrix solve approach."""
@@ -106,15 +111,15 @@ class DisplacementSmoother:
         n = V.shape[0]
         L = self.laplacian_matrix
 
-        # A = L.T @ L + sp.diags([weight] * mesh.n_points) # NOTE: trying to remove the transpose
+        # Solve the system
+        # A = L.T @ L + sp.diags([weight] * self.mesh.n_points)  # NOTE: trying to remove the transpose
         A = L + sp.diags([weight] * self.mesh.n_points)
         b = weight * V
-
-        # Unconstrained smoothing (not recommended—mesh may collapse)
         V_new = np.zeros_like(self.mesh.points)
         for dim in range(3):
             V_new[:, dim] = spla.spsolve(A, b[:, dim])
 
-        smoothed_mesh = self.mesh.copy()
-        smoothed_mesh.points = V_new
-        return smoothed_mesh
+        inter_mesh = self.mesh.copy()
+        inter_mesh.points = V_new
+
+        return inter_mesh
